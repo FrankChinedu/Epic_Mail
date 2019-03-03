7a978738a4381e9950acb3be29b00d92ffd3c237
obi chinedu frank <frankieetchy@gmail.com>
2019-03-03 17:42:39 +0100

    save

--- server/app.js
+++ server/app.js
@@ -2,12 +2,12 @@ import 'dotenv/config';
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-// import morgan from 'morgan';
+import morgan from 'morgan';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(morgan('combined'));
+app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
 
