31863ecc5a99c2e8825a9953dd240cdab096ec3f
obi chinedu frank <frankieetchy@gmail.com>
2019-03-12 19:12:52 +0100

    implemented feedback

--- server/db/index.js
+++ server/db/index.js
@@ -4,17 +4,15 @@ const connectionString = process.env.DEV_DB;
 
 const pool = new Pool({ connectionString });
 
-const query = (text, params) => {
-  return new Promise((resolve, reject) => {
-    pool.query(text, params)
-      .then((res) => {
-        resolve(res);
-      })
-      .catch((err) => {
-        reject(err);
-      });
-  });
-} 
+const query = (text, params) =>  new Promise((resolve, reject) => {
+  pool.query(text, params)
+    .then((res) => {
+      resolve(res);
+    })
+    .catch((err) => {
+      reject(err);
+    });
+});
 
 export default {
   query,
